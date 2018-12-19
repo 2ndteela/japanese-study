23688808bc40b97e365100a7f32e84a300799c65
Jeremy Teela <jteela@ls.byu.edu>
2018-12-19 14:12:16 -0700

    still working on it

--- src/App.js
+++ src/App.js
@@ -1,25 +1,169 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import letters from './alphabet.js'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+    
+    this.state = {
+      kana: '',
+      letterList: [],
+      selectedLetter: null,
+      sortedLetters: []
+    }
+  }
+
+  resetMatching() {
+    const arrList = this.state.letterList
+    const arrSorted = this.state.sortedLetters
+
+    arrSorted.forEach(col => {
+      col.forEach(el => {
+        el.showLetter = null
+      })
+    })
+
+    arrList.forEach(letter => {
+      letter.show = true
+    })
+
+    this.setState({
+      letterList: arrList,
+      sortedLetters: arrSorted
+    })
+  }
+
+  setHirigana() {
+    this.setState({
+      kana: 'Hirigana'
+    })
+
+    this.resetMatching()
+  }
+
+  setKatakana() {
+    this.setState({
+      kana: 'Katakana'
+    })
+
+    this.resetMatching()
+  }
+
+  dragLetter(e, symbol) {
+    this.setState({
+      selectedLetter: symbol
+    })
+  }
+
+  onDragOver(e) {
+    e.preventDefault()
+  }
+  
+
+  onDrop(target, col, idx) {
+
+    if(this.state.selectedLetter.eng === target.eng) {
+      const toKill = this.state.selectedLetter
+
+      const temp = this.state.sortedLetters
+      if(this.state.kana === "Hirigana") temp[col][idx].showLetter = target.hir
+      else temp[col][idx].showLetter = target.kan
+
+      const num = this.state.letterList.indexOf(toKill)
+      const arr = this.state.letterList
+
+      arr[num].show = false
+
+      this.setState({
+        letterList: arr
+      })
+    }
+
+    this.setState({
+      selectedLetter: null
+    })
+  }
+
+  makeTable() {
+    if(!this.state.sortedLetters) return null
+    return this.state.sortedLetters.map( (column, col) => {
+      return ( 
+       <div className="table-column" key={column[0].eng}>
+         {
+           column.map((letter, idx) => {
+             return(
+               <div className="letter-holder" 
+                key={letter.eng + '-drop'} 
+                onDragOver={(e) => this.onDragOver(e)}
+                onDrop={(e) => {this.onDrop(letter, col, idx)}}
+               >
+                <span className="letter-to-match">{letter.eng}</span>
+                <div className="match-holder">{letter.showLetter}</div>
+              </div>
+             )
+           })
+         }
+       </div> 
+      )
+    })
+  }
+
+  componentDidMount() {
+    let tempArray = []
+
+    letters.forEach(col => {
+      col.forEach(lett => {
+        lett.show = true
+        tempArray.push(lett)  
+      })
+    })
+
+    for(let i = 0; i < 100; i++) {
+
+      const one = Math.floor(Math.random() * tempArray.length)
+      const two = Math.floor(Math.random() * tempArray.length)
+
+      const flip = tempArray[one]
+      tempArray[one] = tempArray[two]
+      tempArray[two] = flip
+    }
+    
+    this.setState({
+      letterList: tempArray,
+      sortedLetters: letters
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="header">
+          <h2>Kana Matching</h2>
+          <div style={{flexDirection: 'row'}} > 
+            <button style={{marginRight: '8px'}} onClick={() => this.setHirigana()} >Hirigana</button>
+            <button onClick={() => this.setKatakana()} >Katakana</button>
+          </div>
+        </div>
+        <div id="page-body">
+          <div id="page-content">
+          <div id="matching-table">
+            {
+              this.makeTable()
+            }
+            </div>
+            <div id="letters-to-match">
+              {
+                  this.state.letterList.map(char => {
+
+                    if(this.state.kana === 'Hirigana' && char.show) return <div className="symbol" onTouchMove={(e) => this.dragLetter(e,char)} onDragStart={(e) => this.dragLetter(e, char)} key={char.eng + '-drag-h'} draggable>{char.hir}</div>
+                    else if(char.show) return <div className="symbol" key={char.eng + '-drag-k'} onTouchMove={(e) => this.dragLetter(e,char)} onDragStart={(e) => this.dragLetter(e, char)} draggable >{char.kan}</div>
+                  })
+              }
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ src/alphabet.js
@@ -0,0 +1,262 @@
+const letters = [
+
+    [
+        {
+            eng: 'Wa',
+            hir: 'わ',
+            kan: 'ワ'
+          }, 
+          { 
+            eng: 'Wo',
+            hir: 'を',
+            kan: 'ヲ'
+          },
+          {
+          eng: 'N',
+          hir: 'ん',
+          kan: 'ン'
+          }
+    ],
+
+    [
+        {
+            eng: 'Ra',
+            hir: 'ら',
+            kan: 'ラ'
+          }, 
+          {
+            eng: 'Re',
+            hir: 'れ',
+            kan: 'レ'
+          }, 
+          {
+            eng: 'Ri',
+            hir: 'り',
+            kan: 'リ'
+          },
+          { 
+            eng: 'Ro',
+            hir: 'ろ',
+            kan: 'ㇿ'
+          },
+          {
+          eng: 'Ru',
+          hir: 'る',
+          kan: 'ル'
+          }
+    ],
+
+    [
+        {
+            eng: 'Ya',
+            hir: 'ま',
+            kan: 'ャ'
+          }, 
+          { 
+            eng: 'Yo',
+            hir: 'も',
+            kan: 'ヨ'
+          },
+          {
+          eng: 'Yu',
+          hir: 'む',
+          kan: 'ユ'
+          }
+    ],
+
+    [
+        {
+            eng: 'Ma',
+            hir: 'ま',
+            kan: 'マ'
+          }, 
+          {
+            eng: 'Me',
+            hir: 'め',
+            kan: 'メ'
+          }, 
+          {
+            eng: 'Mi',
+            hir: 'み',
+            kan: 'ミ'
+          },
+          { 
+            eng: 'Mo',
+            hir: 'も',
+            kan: 'モ'
+          },
+          {
+          eng: 'Mu',
+          hir: 'む',
+          kan: 'ㇺ'
+          }
+    ],
+
+    [
+        {
+            eng: 'Ha',
+            hir: 'は',
+            kan: 'ハ'
+          }, 
+          {
+            eng: 'He',
+            hir: 'へ',
+            kan: 'ㇸ'
+          }, 
+          {
+            eng: 'Hi',
+            hir: 'ひ',
+            kan: 'ㇶ'
+          },
+          { 
+            eng: 'Ho',
+            hir: 'ほ',
+            kan: 'ホ'
+          },
+          {
+          eng: 'Fu',
+          hir: 'ふ',
+          kan: 'フ'
+          }
+    ],
+
+    [
+        {
+            eng: 'Na',
+            hir: 'な',
+            kan: 'ナ'
+          }, 
+          {
+            eng: 'Ne',
+            hir: 'ね',
+            kan: 'ネ'
+          }, 
+          {
+            eng: 'Ni',
+            hir: 'に',
+            kan: 'ニ'
+          },
+          { 
+            eng: 'No',
+            hir: 'の',
+            kan: 'ノ'
+          },
+          {
+          eng: 'Nu',
+          hir: 'ぬ',
+          kan: 'ヌ'
+          }
+    ],
+
+    [
+        {
+            eng: 'Ta',
+            hir: 'た',
+            kan: 'タ'
+          }, 
+          {
+            eng: 'Te',
+            hir: 'て',
+            kan: 'テ'
+          }, 
+          {
+            eng: 'Chi',
+            hir: 'ち',
+            kan: 'チ'
+          },
+          { 
+            eng: 'To',
+            hir: 'と',
+            kan: 'ㇳ'
+          },
+          {
+          eng: 'Tsu',
+          hir: 'つ',
+          kan: 'ッ'
+          }
+    ],
+    
+    [
+        {
+            eng: 'Sa',
+            hir: 'さ',
+            kan: 'サ'
+          }, 
+          {
+            eng: 'Se',
+            hir: 'せ',
+            kan: 'セ'
+          }, 
+          {
+            eng: 'Shi',
+            hir: 'し',
+            kan: 'シ'
+          },
+          { 
+            eng: 'So',
+            hir: 'そ',
+            kan: 'ソ'
+          },
+          {
+          eng: 'Su',
+          hir: 'す',
+          kan: 'ス'
+          }
+    ],
+  [
+    {
+      eng: 'Ka',
+      hir: 'あ',
+      kan: 'カ'
+    }, 
+    {
+      eng: 'Ke',
+      hir: 'え',
+      kan: 'ケ'
+    }, 
+    {
+      eng: 'Ki',
+      hir: 'い',
+      kan: 'キ'
+    },
+    { 
+      eng: 'Ko',
+      hir: 'お',
+      kan: 'コ'
+    },
+    {
+    eng: 'Ku',
+    hir: 'う',
+    kan: 'ク'
+    }
+  ],
+  [
+    {
+      eng: 'A',
+      hir: 'あ',
+      kan: 'ア'
+    }, 
+    {
+      eng: 'E',
+      hir: 'え',
+      kan: 'エ'
+    }, 
+    {
+      eng: 'I',
+      hir: 'い',
+      kan: 'ィ'
+    },
+    { 
+      eng: 'O',
+      hir: 'お',
+      kan: 'オ'
+    },
+    {
+    eng: 'U',
+    hir: 'う',
+    kan: 'ウ'
+    }
+  ]
+]
+
+export default letters
